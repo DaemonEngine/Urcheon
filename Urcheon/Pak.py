1eb6487e083a42dd1a7775460cc9353f9224583a
Thomas Debesse <dev@illwieckz.net>
2017-02-18 08:12:40 +0100

    use regexp to parse action list
    - use regexp ro parse action list
    - entensible way to declare unparallel actions

--- Urcheon/Pak.py
+++ Urcheon/Pak.py
@@ -13,18 +13,19 @@ from Urcheon.SourceTree import Inspector
 from Urcheon.SourceTree import PakConfig
 from Urcheon.MapCompiler import BspCompiler
 import __main__ as m
+import argparse
+import configparser
+import fnmatch
+import logging
+import multiprocessing
 import os
-import sys
+import re
 import shutil
 import subprocess
-import argparse
-import logging
-import fnmatch
-import zipfile
-import configparser
+import sys
 import tempfile
 import threading
-import multiprocessing
+import zipfile
 from collections import OrderedDict
 
 
@@ -35,11 +36,11 @@ from collections import OrderedDict
 ui = Ui()
 
 
-class BuildList():
+class ActionList():
 	def __init__(self, source_dir, game_name):
 		self.source_dir = source_dir
-		self.pak_list_file_name = ".pakinfo" + os.path.sep + "build.tsv"
-		self.pak_list_file = self.source_dir + os.path.sep + self.pak_list_file_name
+		self.action_list_file_name = ".pakinfo" + os.path.sep + "actions.txt"
+		self.action_list_file = self.source_dir + os.path.sep + self.action_list_file_name
 
 		self.blacklist = [
 			"Thumbs.db",
@@ -68,41 +69,51 @@ class BuildList():
 
 		self.inspector = Inspector(game_name)
 		self.active_action_dict = OrderedDict()
-		self.inactive_action_dict = OrderedDict()
+		self.disabled_action_dict = OrderedDict()
 		self.computed_active_action_dict = OrderedDict()
-		self.computed_inactive_action_dict = OrderedDict()
+		self.computed_disabled_action_dict = OrderedDict()
 
 		# I want lines printed in this order
 		for action_name in self.inspector.action_name_dict.keys():
 			self.active_action_dict[action_name] = []
-			self.inactive_action_dict[action_name] = []
+			self.disabled_action_dict[action_name] = []
 			self.computed_active_action_dict[action_name] = []
-			self.computed_inactive_action_dict[action_name] = []
+			self.computed_disabled_action_dict[action_name] = []
 
 	def readActions(self):
-		if os.path.isfile(self.pak_list_file):
-			pak_list_file = open(self.pak_list_file, "r")
-			line_list = [line.strip() for line in pak_list_file]
-			pak_list_file.close()
-			for line in line_list:
-				# TODO: regex
-				read_action = line.split('\t')[0]
-				file_path = line[len(read_action) + 1:]
+		if os.path.isfile(self.action_list_file):
+			action_list_file = open(self.action_list_file, "r")
+			line_list = [line.strip() for line in action_list_file]
+			action_list_file.close()
+			action_line_pattern = re.compile(r"^[ \t]*(?P<action_name>[^ \t]*)[ \t]*(?P<file_path>.*)$")
+			quoted_path_pattern = re.compile(r"^\"(?P<file_path>.*)\"[ \t]*$")
+			disabled_action_pattern = re.compile(r"^#[ \t]*(?P<action_name>.*)$")
 
-				if read_action[0] == '#':
-					inactive_action = read_action[1:]
-					ui.print(file_path + ": Known rule, will not " + self.inspector.action_name_dict[read_action] + " (inactive).")
-					self.inactive_action_dict[inactive_action].append(file_path)
+			for line in line_list:
+				line_match = action_line_pattern.match(line)
+				if line_match:
+					action_name = line_match.group("action_name")
+					file_path = line_match.group("file_path")
+
+					file_match = quoted_path_pattern.match(file_path)
+					if file_match:
+						file_path = file_match.group("file_path")
+
+				disabled_action_match = disabled_action_pattern.match(action_name)
+				if disabled_action_match:
+					disabled_action = disabled_action_match.group("action_name")
+					ui.print(file_path + ": Known rule, will not " + disabled_action + " (disabled action).")
+					self.disabled_action_dict[disabled_action].append(file_path)
 				else:
 					if os.path.isfile(file_path):
-						ui.print(file_path + ": Known rule, will " + self.inspector.action_name_dict[read_action] + " (predefined).")
-						self.active_action_dict[read_action].append(file_path)
+						ui.print(file_path + ": Known rule, will " + self.inspector.action_name_dict[action_name] + " (predefined action).")
+						self.active_action_dict[action_name].append(file_path)
 					else:
-						ui.print(file_path + ": Known rule, will not " + self.inspector.action_name_dict[read_action] + " (missing).")
-						self.computed_inactive_action_dict[read_action].append(file_path)
+						ui.print(file_path + ": Known rule, will not " + self.inspector.action_name_dict[action_name] + " (missing file).")
+						self.computed_disabled_action_dict[action_name].append(file_path)
 
 		else:
-			ui.print("List not found: " + self.pak_list_file_name)
+			ui.print("List not found: " + self.action_list_file_name)
 
 	def computeActions(self):
 		for dir_name, subdir_name_list, file_name_list in os.walk(self.source_dir):
@@ -139,41 +150,41 @@ class BuildList():
 				if not blacklisted_file:
 					unknown_file_path = True
 					logging.debug("active actions: " + str(self.active_action_dict))
-					logging.debug("inactive actions:" + str(self.inactive_action_dict))
+					logging.debug("inactive actions:" + str(self.disabled_action_dict))
 					for read_action in self.active_action_dict.keys():
 						if file_path in self.active_action_dict[read_action]:
 							ui.print(file_path + ": Known file, will " + self.inspector.action_name_dict[read_action] + ".")
 							self.computed_active_action_dict[read_action].append(file_path)
 							unknown_file_path = False
-						elif file_path in self.inactive_action_dict[read_action]:
+						elif file_path in self.disabled_action_dict[read_action]:
 							ui.print(file_path + ": Disabled known file, will ignore it.")
-							self.computed_inactive_action_dict[read_action].append(file_path)
+							self.computed_disabled_action_dict[read_action].append(file_path)
 							unknown_file_path = False
 					if unknown_file_path:
 						computed_action = self.inspector.inspect(file_path)
 						self.computed_active_action_dict[computed_action].append(file_path)
 
 		self.active_action_dict = self.computed_active_action_dict
-		self.active_inaction_dict = self.computed_inactive_action_dict
+		self.active_inaction_dict = self.computed_disabled_action_dict
 
 	def writeActions(self):
-		pak_config_subdir = os.path.dirname(self.pak_list_file)
+		pak_config_subdir = os.path.dirname(self.action_list_file)
 		if os.path.isdir(pak_config_subdir):
 			logging.debug("found pakinfo subdir: " +  pak_config_subdir)
 		else:
 			logging.debug("create pakinfo subdir: " + pak_config_subdir)
 			os.makedirs(pak_config_subdir, exist_ok=True)
 
-		pak_list_file = open(self.pak_list_file, "w")
+		action_list_file = open(self.action_list_file, "w")
 		for action in self.active_action_dict.keys():
 			for file_path in sorted(self.active_action_dict[action]):
-				line = action + "\t" + file_path
-				pak_list_file.write(line + "\n")
-		for action in self.computed_inactive_action_dict.keys():
-			for file_path in sorted(self.inactive_action_dict[action]):
-				line = "#" + action + "\t" + file_path
-				pak_list_file.write(line + "\n")
-		pak_list_file.close()
+				line = action + " " + file_path
+				action_list_file.write(line + "\n")
+		for action in self.computed_disabled_action_dict.keys():
+			for file_path in sorted(self.disabled_action_dict[action]):
+				line = "#" + action + " " + file_path
+				action_list_file.write(line + "\n")
+		action_list_file.close()
 
 	def updateActions(self):
 		self.readActions()
@@ -182,25 +193,26 @@ class BuildList():
 
 
 class Builder():
-	def __init__(self, source_dir, build_dir, game_name, map_profile, compute_actions=False):
+	def __init__(self, source_dir, build_dir, game_name, map_profile, auto_actions=False):
 		self.source_dir = source_dir
 		self.build_dir = build_dir
 		self.game_name = game_name
 		self.map_profile = map_profile
-		self.pak_list = BuildList(source_dir, game_name)
+		self.action_list = ActionList(source_dir, game_name)
 
 		# read predefined actions first
-		self.pak_list.readActions()
+		self.action_list.readActions()
 
 		# implicit action list
-		if compute_actions:
-			self.pak_list.computeActions()
+		if auto_actions:
+			self.action_list.computeActions()
 
 		# I want actions executed in this order
 		self.builder_name_dict = OrderedDict()
 		self.builder_name_dict["copy"] =					self.copyFile
-		self.builder_name_dict["merge_bsp"] =				self.mergeBsp
 		self.builder_name_dict["compile_bsp"] =				self.compileBsp
+		# do merge_bsp after copy or compile, this way copied or produced bsp can be modified
+		self.builder_name_dict["merge_bsp"] =				self.mergeBsp
 		self.builder_name_dict["compile_iqm"] =				self.compileIqm
 		self.builder_name_dict["convert_jpg"] =				self.convertJpg
 		self.builder_name_dict["convert_png"] =				self.convertPng
@@ -211,6 +223,11 @@ class Builder():
 		self.builder_name_dict["convert_opus"] =			self.convertOpus
 		self.builder_name_dict["keep"] =					self.keepFile
 		self.builder_name_dict["ignore"] =					self.ignoreFile
+
+		# Actions that can't be parallelized
+		self.linear_actions = []
+		# there can be multiple calls per produced file, it can't be done in parallel
+		self.linear_actions.append("merge_bsp")
 
 		# TODO: set something else in verbose mode
 		self.subprocess_stdout = subprocess.DEVNULL;
@@ -253,7 +270,7 @@ class Builder():
 
 		# TODO: if already exist and older
 		for action in self.builder_name_dict.keys():
-			for file_path in self.pak_list.active_action_dict[action]:
+			for file_path in self.action_list.active_action_dict[action]:
 
 				#TODO: if not source_path
 				source_path = self.getSourcePath(file_path)
@@ -261,9 +278,8 @@ class Builder():
 				# no need to use multiprocessing module to manage task contention, since each task will call its own process
 				# using threads on one core is faster, and it does not prevent tasks to be able to use other cores
 
-				if action in ["merge_bsp"]:
+				if action in self.linear_actions:
 					# action that can't be multithreaded
-					# otherwise merge_bsp is called for every file part but must be called once for all files
 					self.builder_name_dict[action](file_path)
 				else:
 					# threading.Thread's args expect an iterable, hence the comma inside parenthesis otherwise the string is passed as is
@@ -473,10 +489,10 @@ class Builder():
 			ui.verbose("Unmodified file, do nothing: " + file_path)
 			return
 		logging.debug("looking for file in same bspdir than: " + file_path)
-		for sub_path in self.pak_list.active_action_dict["merge_bsp"]:
+		for sub_path in self.action_list.active_action_dict["merge_bsp"]:
 			if sub_path.startswith(bspdir_path):
 				ui.print("Merge to bsp: " + sub_path)
-				self.pak_list.active_action_dict["merge_bsp"].remove(sub_path)
+				self.action_list.active_action_dict["merge_bsp"].remove(sub_path)
 			else:
 				logging.debug("file not from same bspdir: " + sub_path)
 		bsp = Bsp()
@@ -615,9 +631,9 @@ def main(stage=None):
 	args.add_argument("-pf", "--pkg-file", dest="pkg_file", metavar="FILENAME", help="build release pak as file %(metavar)s")
 	args.add_argument("-mp", "--map-profile", dest="map_profile", metavar="PROFILE", default="fast", help="build map with profile %(metavar)s, default: %(default)s")
 	args.add_argument("-ev", "--extra-version", dest="extra_version", metavar="VERSION", help="add %(metavar)s to pak version string")
-	args.add_argument("-u", "--update", dest="update", help="update paklist, compute actions", action="store_true")
+	args.add_argument("-u", "--update-actions", dest="update", help="compute actions, write down list", action="store_true")
 	args.add_argument("-b", "--build", dest="build", help="build source pakdir", action="store_true")
-	args.add_argument("-a", "--auto", dest="compute_actions", help="compute actions at build time and do not store paklist", action="store_true")
+	args.add_argument("-a", "--auto-actions", dest="auto_actions", help="compute actions at build time and do not store the list", action="store_true")
 	args.add_argument("-p", "--package", dest="package", help="compress release pak", action="store_true")
 	args.add_argument("-c", "--clean", dest="clean", help="clean previous build", action="store_true")
 
@@ -636,8 +652,8 @@ def main(stage=None):
 		ui.verbosely = True
 
 	if args.update:
-		pak_list = BuildList(args.source_dir, args.game_profile)
-		pak_list.updateActions()
+		action_list = ActionList(args.source_dir, args.game_profile)
+		action_list.updateActions()
 
 	if args.package or args.build or args.clean:
 		if args.build_prefix:
@@ -672,8 +688,8 @@ def main(stage=None):
 			test_dir = build_prefix + os.path.sep + test_parent_dir + os.path.sep + pak_name + "_test" + os.path.extsep + "pk3dir"
 
 	if args.build:
-		if args.compute_actions:
-			builder = Builder(args.source_dir, test_dir, args.game_profile, args.map_profile, compute_actions=True)
+		if args.auto_actions:
+			builder = Builder(args.source_dir, test_dir, args.game_profile, args.map_profile, auto_actions=True)
 		else:
 			builder = Builder(args.source_dir, test_dir, args.game_profile, args.map_profile)
 
