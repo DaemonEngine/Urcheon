59baa78691d7c120a6983ef83967bafac0118ff0
Thomas Debesse <dev@illwieckz.net>
2017-02-18 21:54:07 +0100

    add fancy couloured output on interactive terminals

--- Urcheon/Ui.py
+++ Urcheon/Ui.py
@@ -8,6 +8,7 @@
 # 
 
 import sys
+from colorama import Fore, Style, init
 
 # keep an eye on the default Python's print function
 _print = print
@@ -16,18 +17,31 @@ verbosely = False
 
 def print(message):
 	# I duplicate print() because I will add colouring support and verbose/quiet support in the future
+	if sys.stdout.isatty():
+		message = Fore.GREEN + message + Style.RESET_ALL
 	_print(message)
 
 def verbose(message):
 	if verbosely:
+		if sys.stdout.isatty():
+			message = Style.DIM + message + Style.RESET_ALL
 		_print(message)
 
 def warning(message):
-	_print("Warning: " + message)
+	message = "Warning: " + message
+	if sys.stdout.isatty():
+		message = Fore.YELLOW + message + Style.RESET_ALL
+	_print(message)
 
 def notice(message):
-	_print("Notice: " + message)
+	message = "Notice: " + message
+	if sys.stdout.isatty():
+		message = Style.BRIGHT + message + Style.RESET_ALL
+	_print(message)
 
 def error(message):
-	_print("Error: " + message)
+	message = "Error: " + message
+	if sys.stdout.isatty():
+		message = Fore.RED + message + Style.RESET_ALL
+	_print(message)
 	raise ValueError(message)
